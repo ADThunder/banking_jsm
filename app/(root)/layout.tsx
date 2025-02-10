2837e62525cff651da73474df258af80d216b2c7
ADThunder <maianhdung02051996@gmail.com>
2025-02-11 00:36:30 +0700

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,28 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 interface RootLayout {
   children: React.ReactNode;
 }
 
 const RootLayout = ({ children }: RootLayout) => {
+  const loggedIn = { firsName: "AD", lastName: "Thunder" };
+
   return (
-    <main>
-      sidebar
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      {/* MOBILE NAV */}
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 };
